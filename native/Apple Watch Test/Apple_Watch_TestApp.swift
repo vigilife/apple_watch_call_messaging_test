d6bcd7fe507443a11e343b3b81515bc7e8849a12
Rexios <rexios@rexios.dev>
2023-01-31 18:38:44 -0500

    Created native app

---
+++ native/Apple Watch Test/Apple_Watch_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Watch_TestApp.swift
+//  Apple Watch Test
+//
+//  Created by Rexios on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Watch_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
