b4d8996e8d35d00dc00b7ea86b3dcdd1d6fab6e6
Rexios <rexios@rexios.dev>
2023-01-31 18:47:13 -0500

    Added watch app

--- native/Apple Watch Test/ContentView.swift
+++ native/Apple Watch Test/ContentView.swift
@@ -6,21 +6,30 @@
 //
 
 import SwiftUI
+import WatchConnectivity
 
 struct ContentView: View {
+    @ObservedObject var controller = Controller()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("\(controller.count)")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class Controller: NSObject, ObservableObject, WCSessionDelegate {
+    @Published var count = 0
+    
+    override init() {
+        super.init()
+        WCSession.default.delegate = self
+        WCSession.default.activate()
     }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        count = message["count"] as! Int
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+    func sessionDidBecomeInactive(_ session: WCSession) {}
+    func sessionDidDeactivate(_ session: WCSession) {}
 }
---
+++ native/Watch Test Watch App/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Watch Test Watch App
+//
+//  Created by Rexios on 1/31/23.
+//
+
+import HealthKit
+import SwiftUI
+import WatchConnectivity
+
+struct ContentView: View {
+    @ObservedObject var controller = Controller()
+
+    var body: some View {
+        Button(action: controller.toggle) {
+            Text(controller.started ? "Stop" : "Start")
+        }
+        TimelineView(PeriodicTimelineSchedule(from: Date(), by: 1)) { _ in
+            Text("\(controller.count)")
+        }
+    }
+}
+
+class Controller: NSObject, ObservableObject, WCSessionDelegate {
+    @Published var started = false
+    @Published var count = 0
+    var session: HKWorkoutSession?
+
+    override init() {
+        super.init()
+        WCSession.default.delegate = self
+        WCSession.default.activate()
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: send)
+    }
+
+    func send(timer: Timer) {
+        count += 1
+        print("sending \(count)")
+        WCSession.default.sendMessage(["count": count], replyHandler: nil)
+    }
+
+    func toggle() {
+        Task { await toggleAsync() }
+    }
+
+    private func toggleAsync() async {
+        if started {
+            session?.end()
+            DispatchQueue.main.async { self.started = false }
+        } else {
+            let healthStore = HKHealthStore()
+            try? await healthStore.requestAuthorization(
+                toShare: [],
+                read: [HKQuantityType.quantityType(forIdentifier: .heartRate)!]
+            )
+            session = try? HKWorkoutSession(healthStore: healthStore, configuration: HKWorkoutConfiguration())
+            session?.startActivity(with: Date())
+            session?.pause()
+            DispatchQueue.main.async { self.started = true }
+        }
+    }
+
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+}
